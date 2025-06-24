856c755f2882d4a6b9215f893fab4493e4b3d42c
JakeOJeff <realjakeojeff@gmail.com>
2025-06-24 09:19:07 +0530

    Init for buttons

--- src/libraries/button.lua
+++ src/libraries/button.lua
@@ -2,18 +2,28 @@ local class = require 'middleclass'
 local Button = class('Button')
 
 
-function Button:initialize(x, y, width, height, text, callback)
+function Button:initialize(x, y, width, height, text, callback, color, hover, duration, ext)
     self.x = x or 10
     self.y = y or 10
     self.width = width or 50
     self.height = height or 20
     self.text = text or "Button"
     self.callback = callback or function() end
+    self.color = color
+    self.ext = loadstring(ext) or loadstring("") -- string -> func
+    self.hover = {
+        x = hover.x or self.x,
+        y = hover.y or self.y,
+        width = hover.width or self.width,
+        height = hover.height or self.height,
+        text = hover.text or self.text,
+        color = hover.color or self.color
+    }
+    self.duration = duration or 0
 end
 
 function Button:update(dt)
-
-
+    
 
 end
 
