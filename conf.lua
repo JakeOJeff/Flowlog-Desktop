74995758e60f22e3c36e167476ea0c7093788466
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-09-04 00:02:17 +0530

    Disabled Console

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.title = "Flowlog V1.2"
-    t.console = true
+    t.console = false
     t.window.width = 700
     t.window.height = 700
     t.window.borderless = true
